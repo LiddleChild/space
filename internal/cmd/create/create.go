6162585cc444d046ec3511dcdc0782a61496de9e
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-09-17 09:39:23 +0700

    feat: create prompt

--- internal/cmd/create/create.go
+++ internal/cmd/create/create.go
@@ -1,22 +1,52 @@
 package create
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/LiddleChild/space/internal/config"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
 var CreateCmd = &cobra.Command{
 	Use:   "create <name>",
 	Short: "create workspace at working directory",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.RangeArgs(0, 1),
 	Run: func(cmd *cobra.Command, args []string) {
-		name := args[0]
+		var name string
 
-		err := config.AppConfig.CreateSpace(name)
+		var err error
+		if len(args) == 0 {
+			name, err = inputPrompt()
+			cobra.CheckErr(err)
+		} else {
+			name = args[0]
+		}
+
+		err = config.AppConfig.CreateSpace(name)
 		cobra.CheckErr(err)
 
 		fmt.Println(name)
 	},
+}
+
+func inputPrompt() (string, error) {
+	input := promptui.Prompt{
+		Label: "space name",
+		Validate: func(input string) error {
+			_, err := config.AppConfig.GetSpace(input)
+			if err != nil {
+				return nil
+			}
+			return errors.New("space name already exists")
+		},
+	}
+
+	result, err := input.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
 }
